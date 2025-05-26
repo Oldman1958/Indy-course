6ee1854c158d72fbbbeeb0c876c48249d0743836
Oldman <boris_i@bk.ru>
2025-05-26 21:20:47 +0500

    Update 3_1_step22.py

--- tests/3_1_step22.py
+++ tests/3_1_step22.py
@@ -9,8 +9,8 @@
 
 black = ['a1', 'a3', 'a5', 'a7', 'b2', 'b4', 'b6', 'b8',
          'c1', 'c3', 'c5', 'c7', 'd2', 'd4', 'd6', 'd8',
-         'e1', 'e3', 'e5', 'e7', 'f2', 'f4', 'f6', 'f8'
-                                                   'g1', 'g3', 'g5', 'g7', 'h2', 'h4', 'h6', 'h8']
+         'e1', 'e3', 'e5', 'e7', 'f2', 'f4', 'f6', 'f8',
+         'g1', 'g3', 'g5', 'g7', 'h2', 'h4', 'h6', 'h8']
 x1 = input()
 x2 = input()
 if (x1 not in black and x2 in black) or (x2 not in black and x1 in black):
