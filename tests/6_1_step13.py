12a6fde7202587ef65bc27bc3711e2c9077d2b23
Oldman1958 <boris_i@bk.ru>
2025-07-04 19:04:54 +0500

    Решение заданий

---
+++ tests/6_1_step13.py
@@ -0,0 +1,8 @@
+"""
+ Создайте список i_love_none из 50 элементов None и распечатайте его.
+"""
+
+i_love_none = []  # создаем пустой список
+for i in range(50):  # циклом мы 50 раз добавляем в список значение None
+    i_love_none.append(None)
+print(i_love_none)
