6f751473d4fad8a31b040f2e33722eca80ed9a24
Oldman1958 <boris_i@bk.ru>
2025-07-05 17:25:34 +0500

    Решение заданий

---
+++ tests/6_3_step14.py
@@ -0,0 +1,22 @@
+"""
+Дан список игроков players, где каждый элемент — кортеж из имени игрока и его силы.
+К примеру:
+
+players = [("Alice", 10), ("Bob", 15), ("Charlie", 20), ("Dave", 25)]
+Игроки могут сражаться друг с другом, если разница их силы не превышает заданный параметр max_difference.
+
+Напишите программу, которая выводит все возможные битвы, удовлетворяющие этому условию.
+
+Сами переменные players и max_difference определяются во входных данных. Вам вводить ничего не нужно.
+"""
+
+players = [("Alice", 10), ("Bob", 15), ("Charlie", 20), ("Dave", 25)]
+max_difference = 6
+
+for i in range(len(players)):
+    for j in range(i, len(players)):
+        if i == j:
+            continue
+        else:
+            if abs(players[i][1] - players[j][1]) <= max_difference:
+                print(f"{players[i][0]} vs {players[j][0]}")
