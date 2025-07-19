abcb9b93d0ca48c72e32cf5e73ec7758a740dc7b
Oldman1958 <boris_i@bk.ru>
2025-07-19 10:49:31 +0500

    Решение заданий

---
+++ tests/6_4_step15.py
@@ -0,0 +1,17 @@
+"""
+Перед вами имеется словарь sweet
+
+В отдельных строках распечатайте сперва значение ключа name,
+потом calories и напоследок id
+"""
+
+sweet = {
+    "id": "0001",
+    "type": "donut",
+    "name": "Cake",
+    "ppu": 0.55,
+    "calories": 125,
+}
+print(sweet["name"])
+print(sweet["calories"])
+print(sweet["id"])
