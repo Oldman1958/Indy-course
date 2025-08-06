56c701211673f68f750158ad7b623d3cd0256c00
Oldman1958 <boris_i@bk.ru>
2025-08-06 11:43:27 +0500

    Решение заданий

---
+++ tests/6_12_step11.py
@@ -0,0 +1,23 @@
+"""
+Перед вами располагается список words, состоящий из 55 слов, которые могут повторяться
+
+Ваша задача вывести на экран количество уникальных слов, длина которых больше 6.
+
+Под уникальностью здесь подразумевается то, что в случае возникновения дублирующих слов,
+в подсчете вы не должны учитывать дубли.
+"""
+
+words = ['mention', 'soup', 'pneumonia', 'tradition', 'concert', 'tease', 'generation',
+         'winter', 'national', 'jacket', 'winter', 'wrestle', 'proposal', 'error',
+         'pneumonia', 'concert', 'value', 'value', 'disclose', 'glasses', 'tank',
+         'national', 'soup', 'feel', 'few', 'concert', 'wrestle', 'proposal', 'soup',
+         'sail', 'brown', 'service', 'proposal', 'winter', 'jacket', 'mention', 'tradition',
+         'value', 'feel', 'bear', 'few', 'value', 'winter', 'proposal', 'government',
+         'control', 'value', 'few', 'generation', 'service', 'national',
+         'tradition', 'government', 'mention', 'proposal']
+words_set = set(words)
+count = 0
+for item in words_set:
+    if len(item) > 6:
+        count += 1
+print(count)
